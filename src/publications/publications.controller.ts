69fbdd02f62e3d98861c7c985a51b606087e545c
Dontcode44 <kabutto378@gmail.com>
2022-11-18 12:05:38 -0500

    [ADD] - Publication post funcionality 👾

--- src/publications/publications.controller.ts
+++ src/publications/publications.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Userd } from 'src/auth/decorators/token.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/users/entities/user.entity';
+import { CreatePublicationDto } from './dto/create-publication.dto';
 import { PublicationsService } from './publications.service';
 
 @Controller('publications')
 export class PublicationsController {
   constructor(private readonly publicationsService: PublicationsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.publicationsService.findAll();
   }
-  
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createPublication(
+    @Userd() user: User,
+    @Body() createPublication: CreatePublicationDto,
+  ) {
+    return await this.publicationsService.createPublication(
+      user.id,
+      createPublication,
+    );
+  }
 }
