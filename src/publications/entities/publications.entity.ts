56dba7c4429f702f06a1b0a007117d170a114ba3
Dontcode44 <kabutto378@gmail.com>
2022-12-01 20:33:07 -0500

    [ADD] - New index on publication

--- src/publications/entities/publications.entity.ts
+++ src/publications/entities/publications.entity.ts
@@ -1,12 +1,14 @@
 import { Profiles } from "src/profiles/entities/profiles.entity";
 import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
+@Entity({
+  name: 'publication',
+})
 export class Publications {
   @PrimaryGeneratedColumn('identity')
   id: string;
 
-  @Index({})
+  @Index('idx_title')
   @Column()
   title: string;
 
@@ -17,8 +19,12 @@ export class Publications {
   likes: number;
 
   @ManyToOne(() => Profiles, (profile) => profile.publications)
+  @Index('idx_author')
   author: Profiles;
 
+  /* Creating a column in the database that is an array of strings that
+  would be Public, Private, or Friends.
+  */
   @Column({ type: 'simple-array' })
   privacity: string[];
 }
